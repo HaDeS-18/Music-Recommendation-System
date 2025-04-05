a0e05296f524d5bf7f81a5acb83061fd6cc0ca6e
nitesh kakkar <kakkar.nitesh04@gmail.com>
2025-04-05 19:29:30 +0530

    feat: spotify integration based on model results

--- beat-wise/app/test/page.tsx
+++ beat-wise/app/test/page.tsx
@@ -1,9 +1,9 @@
-import SpotifySearch from './components/SpotifySearch';
+import NormalUISearch from "../components/normalUISearch";
 
 export default function TestPage() {
   return (
     <main className="min-h-screen flex flex-col items-center justify-center p-4">
-      <SpotifySearch />
+      <NormalUISearch />
     </main>
   );
 }
